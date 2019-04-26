dcf6d63085b33f7cb05aa6f89702b26190d24f04
dymx101 <yimingdong@Yimings-MacBook-Pro-2.local>
2019-04-27 00:03:55 +0800

    image view style

--- codeChallengeApp/codeChallengeApp/Screens/PhotoList/Views/PhotoListItemCell.swift
+++ codeChallengeApp/codeChallengeApp/Screens/PhotoList/Views/PhotoListItemCell.swift
@@ -7,16 +7,28 @@
 //
 
 import UIKit
+import Kingfisher
 
 class PhotoListItemCell: BaseCollectionViewCell {
     
-    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var imageView: UIImageView! {
+        didSet {
+            imageView.layer.cornerRadius = 4
+            imageView.layer.masksToBounds = true
+        }
+    }
     
-    var viewModel: PhotoListItemCellViewModelProtocol?
+    var viewModel: PhotoListItemCellViewModelProtocol? {
+        didSet {
+            if let urlString = viewModel?.imageUrl, let url = URL(string: urlString) {
+                imageView.kf.indicatorType = .activity
+                imageView.kf.setImage(with: url, options: [.transition(.fade(0.2))])
+            }
+        }
+    }
     
     override func awakeFromNib() {
         super.awakeFromNib()
-        // Initialization code
     }
 
 }
